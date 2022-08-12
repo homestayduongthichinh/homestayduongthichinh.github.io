f4460270ecea40a9541edbd1bcf5c775dff6021b
DaiDH <40824445+id1945@users.noreply.github.com>
2022-08-12 14:33:13 +0700

    Update load-html.js

--- shared/load-html.js
+++ shared/load-html.js
@@ -3,4 +3,28 @@ $(document).ready(function() {
   $("#nav").load("/shared/nav.html");
   // Footer  
   $("#footer").load("/shared/footer.html");
+  
+  $.ajax({
+    type: "GET",// method
+    url: `/shared/nav.html`, // route
+    data: {
+      "key":"value" // json format in key value pair
+    },
+    contentType: "application/json;charset=UTF-8",
+    success: function (res) {
+        $("#nav").html(res);
+     }
+  });
+  
+  $.ajax({
+    type: "GET",// method
+    url: `/shared/footer.html`, // route
+    data: {
+      "key":"value" // json format in key value pair
+    },
+    contentType: "application/json;charset=UTF-8",
+    success: function (res) {
+        $("#footer").html(res);
+     }
+  });
 });
