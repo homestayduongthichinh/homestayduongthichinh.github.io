e06609c1c8cd9831d03b6fb3621aadde357939c7
daidh1 <daidh@goline.vn>
2022-11-06 18:29:12 +0700

    update

--- shared/load-html.js
+++ shared/load-html.js
@@ -1,39 +1,39 @@
-(function () {
+(async function () {
   // innerHTML
   const innerHTML = (text, id) => {
     const el = document.getElementById(id);
-    if (el) {
+    if (el && !el.hasChildNodes()) {
       el.innerHTML = text;
     }
   };
 
   // nav
-  fetch("/shared/nav.html")
+  await fetch("/shared/nav.html")
     .then((response) => response.text())
     .then((text) => innerHTML(text, "nav"));
 
   // footer
-  fetch("/shared/footer.html")
+  await fetch("/shared/footer.html")
     .then((response) => response.text())
     .then((text) => innerHTML(text, "footer"));
 
   // categories
-  fetch("/shared/posts/inner_categories.html")
+  await fetch("/shared/posts/inner_categories.html")
     .then((response) => response.text())
     .then((text) => innerHTML(text, "inner_categories"));
 
   // recent
-  fetch("/shared/posts/inner_recent.html")
+  await fetch("/shared/posts/inner_recent.html")
     .then((response) => response.text())
     .then((text) => innerHTML(text, "inner_recent"));
 
   // tags
-  fetch("/shared/posts/inner_tags.html")
+  await fetch("/shared/posts/inner_tags.html")
     .then((response) => response.text())
     .then((text) => innerHTML(text, "inner_tags"));
 
   // comments
-  fetch("/shared/posts/post_comments.html")
+  await fetch("/shared/posts/post_comments.html")
     .then((response) => response.text())
     .then((text) => innerHTML(text, "post_comments"));
 })();
