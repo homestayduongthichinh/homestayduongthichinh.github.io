c35f5c1c43d0b79b672fa85c6a29ef790bf2fa29
daidh1 <daidh@goline.vn>
2022-11-06 14:09:02 +0700

    update

--- shared/load-html.js
+++ shared/load-html.js
@@ -1,61 +1,71 @@
 (function () {
+  const timeoutInterval = (i) => setTimeout(() => clearInterval(i), 2000);
+  // innerHTML
+  const innerHTML = (interval, text, cb) => {
+    console.log(interval)
+    if (cb()) {
+      cb().innerHTML = text;
+      clearInterval(interval);
+    }
+  };
+
   // nav
   fetch("/shared/nav.html")
     .then((response) => response.text())
     .then((text) => {
-      const el = document.getElementById("nav");
-      if (el) {
-        el.innerHTML = text;
-      }
+      const loop = setInterval(() => {
+        innerHTML(loop, text, () => document.getElementById("nav"));
+      }, 0);
+      timeoutInterval(loop);
     });
 
   // footer
   fetch("/shared/footer.html")
     .then((response) => response.text())
     .then((text) => {
-      const el = document.getElementById("footer");
-      if (el) {
-        el.innerHTML = text;
-      }
+      const loop = setInterval(() => {
+        innerHTML(loop, text, () => document.getElementById("footer"));
+      }, 0);
+      timeoutInterval(loop);
     });
 
   // categories
   fetch("/shared/posts/inner_categories.html")
     .then((response) => response.text())
     .then((text) => {
-      const nav = document.getElementById("inner_categories");
-      if (nav) {
-        nav.innerHTML = text;
-      }
+      const loop = setInterval(() => {
+        innerHTML(loop, text, () => document.getElementById("inner_categories"));
+      }, 0);
+      timeoutInterval(loop);
     });
 
   // recent
   fetch("/shared/posts/inner_recent.html")
     .then((response) => response.text())
     .then((text) => {
-      const el = document.getElementById("inner_recent");
-      if (el) {
-        el.innerHTML = text;
-      }
+      const loop = setInterval(() => {
+        innerHTML(loop, text, () => document.getElementById("inner_recent"));
+      }, 0);
+      timeoutInterval(loop);
     });
 
   // tags
   fetch("/shared/posts/inner_tags.html")
     .then((response) => response.text())
     .then((text) => {
-      const el = document.getElementById("inner_tags");
-      if (el) {
-        el.innerHTML = text;
-      }
+      const loop = setInterval(() => {
+        innerHTML(loop, text, () => document.getElementById("inner_tags"));
+      }, 0);
+      timeoutInterval(loop);
     });
 
   // comments
   fetch("/shared/posts/post_comments.html")
     .then((response) => response.text())
     .then((text) => {
-      const el = document.getElementById("post_comments");
-      if (el) {
-        el.innerHTML = text;
-      }
+      const loop = setInterval(() => {
+        innerHTML(loop, text, () => document.getElementById("post_comments"));
+      }, 0);
+      timeoutInterval(loop);
     });
 })();
