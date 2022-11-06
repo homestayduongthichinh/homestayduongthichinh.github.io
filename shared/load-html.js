09578cfa09dcf4b26255a78672cfcf6c793a17cb
daidh1 <daidh@goline.vn>
2022-11-06 12:12:08 +0700

    update

--- shared/load-html.js
+++ shared/load-html.js
@@ -1,61 +1,61 @@
 (function () {
   // nav
   fetch("/shared/nav.html")
-      .then((response) => response.text())
-      .then((text) => {
-          const nav = document.getElementById("nav");
-          if (nav) {
-              nav.innerHTML = text;
-          }
-      });
+    .then((response) => response.text())
+    .then((text) => {
+      const el = document.getElementById("nav");
+      if (el) {
+        el.innerHTML = text;
+      }
+    });
 
   // footer
   fetch("/shared/footer.html")
-      .then((response) => response.text())
-      .then((text) => {
-          const nav = document.getElementById("footer");
-          if (nav) {
-              nav.innerHTML = text;
-          }
-      });
+    .then((response) => response.text())
+    .then((text) => {
+      const el = document.getElementById("footer");
+      if (el) {
+        el.innerHTML = text;
+      }
+    });
 
   // categories
   fetch("/shared/posts/inner_categories.html")
-      .then((response) => response.text())
-      .then((text) => {
-          const nav = document.getElementById("inner_categories");
-          if (nav) {
-              nav.innerHTML = text;
-          }
-      });
+    .then((response) => response.text())
+    .then((text) => {
+      const nav = document.getElementById("inner_categories");
+      if (nav) {
+        nav.innerHTML = text;
+      }
+    });
 
   // recent
   fetch("/shared/posts/inner_recent.html")
-      .then((response) => response.text())
-      .then((text) => {
-          const nav = document.getElementById("inner_recent");
-          if (nav) {
-              nav.innerHTML = text;
-          }
-      });
+    .then((response) => response.text())
+    .then((text) => {
+      const el = document.getElementById("inner_recent");
+      if (el) {
+        el.innerHTML = text;
+      }
+    });
 
   // tags
   fetch("/shared/posts/inner_tags.html")
-      .then((response) => response.text())
-      .then((text) => {
-          const nav = document.getElementById("inner_tags");
-          if (nav) {
-              nav.innerHTML = text;
-          }
-      });
+    .then((response) => response.text())
+    .then((text) => {
+      const el = document.getElementById("inner_tags");
+      if (el) {
+        el.innerHTML = text;
+      }
+    });
 
   // comments
   fetch("/shared/posts/post_comments.html")
-      .then((response) => response.text())
-      .then((text) => {
-          const nav = document.getElementById("post_comments");
-          if (nav) {
-              nav.innerHTML = text;
-          }
-      });
+    .then((response) => response.text())
+    .then((text) => {
+      const el = document.getElementById("post_comments");
+      if (el) {
+        el.innerHTML = text;
+      }
+    });
 })();
